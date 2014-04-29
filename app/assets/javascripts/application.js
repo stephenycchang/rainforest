be0ca81ebc96ae51f5154c9dfe72d76a8580409a
Stephen Chang <stephenycchang@gmail.com>
2014-04-29 16:52:18 -0400

    Changed to Ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,22 +14,37 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(document).ready(function()){
-  function display_search_results() {
-    if (this.readyState === 4) {
-      console.log(this);
-      document.getElementById('products').innerHTML = this.responseText;
-    };
-  }
-}
+// $(document).ready(function()){
+//   function display_search_results() {
+//     if (this.readyState === 4) {
+//       console.log(this);
+//       document.getElementById('products').innerHTML = this.responseText;
+//     };
+//   }
+// }
 
-var form = document.getElementById('serach-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var searchValue = document.getElementById('search').value;
+// var form = document.getElementById('serach-form');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   var searchValue = document.getElementById('search').value;
 
- var xhr = new XMLHtttpRequest();
-  xhr.onload = display_search_results;
-  xhr.open('GET', '/products/search?search=' + searchValue, true);
-  xhr.send();
+//  var xhr = new XMLHtttpRequest();
+//   xhr.onload = display_search_results;
+//   xhr.open('GET', '/products/search?search=' + searchValue, true);
+//   xhr.send();
+// });
+
+$(document).ready(function() {
+  $('#search-form').submit(function(event)
+    event.preventDefault();
+    var searchValue = $('search').val();
+
+    $.ajax({
+      url: '/products/search?search=' + searchValue
+      type: 'GET'
+      dataType: 'html'
+    }).done(function(data){
+      $('products').html(data);
+    });
+  });
 });
